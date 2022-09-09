512eddcf8e876223b20f6c4fa98d2a31c52a5823
udeasan <santiago.bedoyao@udea.edu.co>
2022-09-08 19:59:59 -0500

    Allowing cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const cors = require('cors');
 const connectDB = require('./config/db.js');
 const swaggerUi = require('swagger-ui-express')
 const swaggerFile = require('./swagger_output.json')
@@ -8,6 +9,7 @@ connectDB();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
 app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
